fb0d51e131bb982e59a91c31e4e1b8673922bb2e
hasnainroopawalla <hasnain.roopawalla@gmail.com>
2022-07-20 19:43:22 +0530

    Working PSO, Added pre-commit and requirements

--- src/pso.py
+++ src/pso.py
@@ -1,22 +1,27 @@
 from search_space import SearchSpace
 from functions import basic
 
-def error_criteria_attained(g_best_value, target_value, target_error):
-    return abs(g_best_value - target_value) <= target_error
 
-k = 0
 MAX_ITERATIONS = 50
 
-search_space = SearchSpace(fitness_function=basic, target_value=1, target_error=1e-6, num_particles=30)
+search_space = SearchSpace(
+    fitness_function=basic, target_value=1, target_error=1e-6, num_particles=30
+)
 
-while k <  MAX_ITERATIONS:
+k = 0
+while k < MAX_ITERATIONS:
     search_space.set_p_best()
     search_space.set_g_best()
 
-    if error_criteria_attained(search_space.g_best_value, search_space.target_value, search_space.target_error): 
+    if (
+        abs(search_space.g_best_value - search_space.target_value)
+        <= search_space.target_error
+    ):
         break
-    
+
     search_space.move_particles()
     k += 1
-    
-print(f'Iterations: {k}, Best Solution: {search_space.g_best_position}')
+
+print(
+    f"Iterations: {k}, Best Solution: {search_space.g_best_position}, Value: {search_space.g_best_value}"
+)
--- src/search_space.py
+++ src/search_space.py
@@ -1,39 +1,51 @@
 from typing import List
 import numpy as np
-import random
 
 from particle import Particle
 
+
 W = 0.5
-c1, c2 = 0.8, 0.9
+C1, C2 = 0.8, 0.9
+
 
 class SearchSpace:
-    def __init__(self, fitness_function, target_value, target_error, num_particles) -> None:
+    def __init__(
+        self, fitness_function, target_value, target_error, num_particles
+    ) -> None:
+        self.fitness_function = fitness_function
         self.target_value = target_value
         self.target_error = target_error
         self.num_particles = num_particles
-        self.g_best_value = float('inf')
-        self.g_best_position = np.array([random.random()*50, random.random()*50])
+        self.g_best_value = float("inf")
+        self.g_best_position = np.random.uniform(-50, 50, (2,))
         self.particles: List[Particle] = [Particle() for _ in range(num_particles)]
-        self.fitness_function = fitness_function
-        
+
     def set_p_best(self):
         for particle in self.particles:
             fitness_value = self.fitness_function(particle.position)
-            if  particle.p_best_value > fitness_value:
-                particle.p_best_position, particle.p_best_value = particle.position, fitness_value
-    
+            if particle.p_best_value > fitness_value:
+                particle.p_best_position, particle.p_best_value = (
+                    particle.position,
+                    fitness_value,
+                )
+
     def set_g_best(self):
         for particle in self.particles:
             fitness_value = self.fitness_function(particle.position)
             if self.g_best_value > fitness_value:
-                self.g_best_position, self.g_best_value = particle.position, fitness_value
-        print(self.g_best_value)
+                self.g_best_position, self.g_best_value = (
+                    particle.position,
+                    fitness_value,
+                )
+
     def move_particles(self):
         for particle in self.particles:
-            new_velocity = (W * particle.velocity) + \
-                           (c1 * np.random.uniform()) * (particle.p_best_position - particle.position) + \
-                           (c2 * np.random.uniform()) * (self.g_best_position - particle.position)
+            new_velocity = (
+                (W * particle.velocity)
+                + (C1 * np.random.uniform())
+                * (particle.p_best_position - particle.position)
+                + (C2 * np.random.uniform())
+                * (self.g_best_position - particle.position)
+            )
             particle.velocity = new_velocity
             particle.move()
-            
